250890ce1c4c012ab4e4ef38f54954852b429dea
AdetolaAremu <75201167+AdetolaAremu@users.noreply.github.com>
2021-03-29 15:33:09 +0100

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import './assets/style.css';
+
+// import Vuelidate from 'vuelidate'
+
+// const app = createApp(Vuelidate)
+// app.use(Vuelidate)
+
+
+createApp(App).mount('#app')
